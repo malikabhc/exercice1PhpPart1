2a1302c376e5546487a0363fcf9212a8893d9b65
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 08:58:44 +0200

    exercice1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice1_1</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $name = 'Malika';
+      echo $name;
+      ?>
+    </p>
+  </body>
+</html>
